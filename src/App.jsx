641840548d27437c0986c217c16f61c536b1132c
aycman <man.hajilou@gmail.com>
2026-05-08 23:19:58 +0200

    added image and loading text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import logo from './assets/associazione-Apadana-LOGO--low-web.png' // فرض بر این است که فایل در src/assets است
 import './App.css'
 
 function App() {
 
   return (
     <>
-     <div className="div1">Coming soon...</div>
+     
+    <div className="div1">
+      <img src={logo} alt="" />
+      <h1 className="gentle-text">Coming Soon</h1>
+    </div>
+
+
+{/* 
+     <h2 className="pulse-text">Coming soon...</h2>
+
+
+      <div className="container">
+        <div className="text">Coming Soon</div>
+        <div className="progress-bar"></div>
+      </div>
+
+
+      <span className="spinner"></span>
+
+ */}
+
+
+
+
+
+
 
      
     </>
